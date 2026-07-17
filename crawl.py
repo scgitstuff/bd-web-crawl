952cd8a3cf82f2ff59143a55289308ae1eea30d9
scgitstuff <67354101+scgitstuff@users.noreply.github.com>
2026-07-17 16:33:47 -0400

    1.4

--- crawl.py
+++ crawl.py
@@ -1,4 +1,5 @@
 from urllib.parse import urlsplit
+from bs4 import BeautifulSoup, Tag
 
 
 def normalize_url(url: str) -> str:
@@ -6,3 +7,21 @@ def normalize_url(url: str) -> str:
     full_path = f"{parsed_url.netloc}{parsed_url.path}"
     full_path = full_path.rstrip("/")
     return full_path.lower()
+
+
+def get_heading_from_html(html: str) -> str:
+    soup = BeautifulSoup(html, "html.parser")
+    h_tag = soup.find("h1") or soup.find("h2")
+    return h_tag.get_text(strip=True) if isinstance(h_tag, Tag) else ""
+
+
+def get_first_paragraph_from_html(html: str) -> str:
+    soup = BeautifulSoup(html, "html.parser")
+
+    main_section = soup.find("main")
+    if isinstance(main_section, Tag):
+        first_p = main_section.find("p")
+    else:
+        first_p = soup.find("p")
+
+    return first_p.get_text(strip=True) if isinstance(first_p, Tag) else ""
