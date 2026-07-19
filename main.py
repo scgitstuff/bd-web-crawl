0a9b1076048e2e5f269b133a192f2b721e057ba9
scgitstuff <67354101+scgitstuff@users.noreply.github.com>
2026-07-19 16:06:24 -0400

    2.1

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-def main():
-    print("Hello from bd-web-crawl!")
+import sys
+
+
+def main() -> None:
+    args = sys.argv
+    if len(args) < 2:
+        print("no website provided")
+        sys.exit(1)
+    if len(args) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+
+    base_url = args[1]
+
+    print(f"starting crawl of: {base_url}...")
+
+    sys.exit(0)
 
 
 if __name__ == "__main__":
